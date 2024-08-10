0199a31579d2cff57f137a5b19c84fbb68762a10
Rohit-1301 <148363170+Rohit-1301@users.noreply.github.com>
2024-08-10 21:46:09 +0530

    Add files via upload
    This folder is sharing my knowledge and programs of conditional expressions in python.

---
+++ conditional expressions/condition.py
@@ -0,0 +1,15 @@
+# age=int(input("Enter your age: "))
+# if(age>=18):
+#     print("You are eligible to vote")
+
+# elif(age<0):
+#     print("Your age is not valid")
+
+# else :
+#     print("You are not eligible to vote")
+
+post=input("Enter your post: ")
+if("Rohit".lower() in post.lower()):
+    print("Post is talking about Rohit")
+else:
+    print("Post is not talking about Rohit")
