5544fee95e03a286929e65332d0c88fa08544cb5
Rohit-1301 <148363170+Rohit-1301@users.noreply.github.com>
2024-08-10 22:01:16 +0530

    Add files via upload
    This folder is showcasing my knowledge on variables and its datatypes of python.

---
+++ variables and datatypes/practiceset2.py
@@ -0,0 +1,7 @@
+# question 1 is addition of two number
+a=int(input("enter the number u want to add : "))
+b=int(input("enter the number u want to add : "))
+print("the sum of two number is: ",a+b)
+# question 2 is to find remainder of the number
+print("the remainder of two number is: ",a%b)
+print("the square of two number is: ", a**2)
---
+++ variables and datatypes/variable.py
@@ -0,0 +1,16 @@
+a=5
+b=7.8889
+c="harry"
+print(a+b+6)
+d='c'
+print(c)
+print(d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(int(b))
+print(c+d)
+e=input("enter the number u want to add : ")
+f=input("enter the number u want to add : ")
+print("the sum of input of two number  number is : ",int(e)+int(f))
